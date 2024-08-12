58cb7aecfbce0c427c045407baeebc5a418fbafe
edangros <edangros@gmail.com>
2024-08-12 10:57:28 +0900

    preparing: removed modules

--- modules/spring-module/src/main/java/org/simplejavamail/springsupport/package-info.java
+++
@@ -1,7 +0,0 @@
-/**
- * Contains the only exposed API of this module which is the entry class to import into Spring.
- * <p>
- * For more information on this module, please refer to
- * <a href="https://www.simplejavamail.org/modules.html#spring-module">Spring support on simplejavamail.org</a>.
- */
-package org.simplejavamail.springsupport;
